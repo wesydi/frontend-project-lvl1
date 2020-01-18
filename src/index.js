e28b6ed790a6d1ce91fce90631974d334eb6e496
wesydi <51164434+wesydi@users.noreply.github.com>
2020-01-18 21:56:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export const whatsUrName = () => {
-  let userName = readlineSync.question('May I have your name? ')
-console.log('Hi ' + userName + '!')
-}
+const whatsUrName = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hi ${userName}!`);
+};
+export default whatsUrName;
