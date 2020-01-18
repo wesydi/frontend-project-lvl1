1239376e52b144c12390297b0eee53c7e33e141f
wesydi <51164434+wesydi@users.noreply.github.com>
2020-01-18 17:37:51 +0300

    add index.js

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,5 @@
 #!/usr/bin/env node
+import { whatsUrName } from '..';
 
 console.log('Welcome to the Brain Games!');
+whatsUrName();
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+
+export const whatsUrName = () => {
+	let userName = readlineSync.question('May I have your name? ');
+	console.log('Hi ' + userName + '!');
+};
