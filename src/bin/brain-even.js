ead12f72909b3206407e95f5b8a97e0b0924593b
wesydi <51164434+wesydi@users.noreply.github.com>
2020-01-19 13:10:55 +0300

    linter

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import { name, evenNumber } from '..';
 
-console.log(`Welcome to the Brain Games! Answer "yes" if the number is even, otherwise answer "no".`);
+console.log('Welcome to the Brain Games! Answer "yes" if the number is even, otherwise answer "no".');
 console.log(`Hello ${name()}`);
-console.log(evenNumber(`John`));
-
+console.log(evenNumber('John'));
