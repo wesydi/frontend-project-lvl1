16f56b39da84154236a0c09c5409b4f480d7fc4c
wesydi <marsss278@gmail.com>
2020-01-19 22:55:57 +0300

    add edits to work func

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -2,5 +2,6 @@
 import { name, evenNumber } from '..';
 
 console.log('Welcome to the Brain Games! Answer "yes" if the number is even, otherwise answer "no".');
-console.log(`Hello ${name()}`);
-console.log(evenNumber('John'));
+const userName = name();
+console.log(`Hello, ${userName}`);
+console.log(evenNumber(userName));
--- src/index.js
+++ src/index.js
@@ -14,10 +14,15 @@ const evenNumber = (usrName) => {
     if (answerUser === 'yes' && number % 2 === 0 || answerUser === 'no' && number % 2 !== 0) {
       acc += 1;
       console.log(`Correct!`);
-    } else {
-      acc = 0;
-      console.log(`'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again, ${usrName}!`);
     }
+    if (answerUser !== 'yes' && number % 2 === 0) {
+      acc = 1;
+      console.log(`'${answerUser}' is wrong answer ;(. Correct answer was 'yes'. Let's try again, ${usrName}!`);
+  	}
+  	if (answerUser !== 'no' && number % 2 !== 0) {
+      acc = 1;
+      console.log(`'${answerUser}' is wrong answer ;(. Correct answer was 'no'. Let's try again, ${usrName}!`);
+  	}
     return iter(Math.floor(Math.random() * 10), acc);
   };
   return iter(randomNumber, 1);
