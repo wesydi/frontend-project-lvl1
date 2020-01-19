6ecb5372de39481f1e7679e46c7c73ad9dffc239
wesydi <51164434+wesydi@users.noreply.github.com>
2020-01-19 13:14:35 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ const name = () => {
 const evenNumber = (usrName) => {
   const randomNumber = Math.floor(Math.random() * 10);
   const iter = (number, acc) => {
-    const answerUser = readlineSync.question(number);
+    const answerUser = readlineSync.question(`Question: ${number}`);
     if (acc === 3) return `Congratulations, ${usrName}!`;
     if (answerUser === 'yes' && number % 2 === 0 || answerUser === 'no' && number % 2 !== 0) {
       acc += 1;
-      console.log('Correct!');
+      console.log(`Your Answer: ${answerUser}. Correct!`);
     } else {
       acc = 0;
-      console.log(`'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again, ${usrName}!`);
+      console.log(`Your Answer: ${answerUser}. 'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again, ${usrName}!`);
     }
     return iter(Math.floor(Math.random() * 10), acc);
   };
