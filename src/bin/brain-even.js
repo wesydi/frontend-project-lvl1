2753abdd66db1f982fd236c665aa684cb6ed42e3
wesydi <marsss278@gmail.com>
2020-01-20 21:37:35 +0300

    add indent to string

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { name, evenNumber } from '..';
 
-console.log('Welcome to the Brain Games! Answer "yes" if the number is even, otherwise answer "no".');
+console.log('Welcome to the Brain Games!\nAnswer "yes" if the number is even, otherwise answer "no".');
 const userName = name();
-console.log(`Hello, ${userName}`);
+console.log(`Hello, ${userName}\n`);
 console.log(evenNumber(userName));
