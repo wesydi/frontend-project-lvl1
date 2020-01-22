46a6cf1cf3de7a34e1ae5ab7dd778448fcd94d41
wesydi <marsss278@gmail.com>
2020-01-23 01:33:07 +0300

    fix function prime

--- src/bin/brain-prime.js
+++ src/bin/brain-prime.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { name, prime } from '..';
 
-console.log('Welcome to the Brain Games!\nWhat number is missing in the progression?.');
+console.log('Welcome to the Brain Games!\nAnswer "yes" if given number is prime. Otherwise answer "no".');
 const userName = name();
 console.log(`Hello, ${userName}\n`);
 console.log(prime(userName));
--- src/bin/brain-progression.js
+++ src/bin/brain-progression.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { name, progression } from '..';
 
-console.log('Welcome to the Brain Games!\nAnswer "yes" if given number is prime. Otherwise answer "no".');
+console.log('Welcome to the Brain Games!\nWhat number is missing in the progression?.');
 const userName = name();
 console.log(`Hello, ${userName}\n`);
 console.log(progression(userName));
