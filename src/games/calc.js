2c771d1c14840b3e875bd6f2338dfeceb23f5a25
wesydi <marsss278@gmail.com>
2020-02-04 15:32:42 +0300

    fix name of calculation func

--- src/games/calc.js
+++ src/games/calc.js
@@ -5,24 +5,24 @@ const gameMessage = 'What is the result of the expression?';
 
 const operations = ['*', '+', '-'];
 
-const calculator = (arg1, arg2, operator) => {
+const calculation = (number1, number2, operator) => {
   switch (operator) {
-    case '*': return arg1 * arg2;
-    case '+': return arg1 + arg2;
-    case '-': return arg1 - arg2;
+    case '*': return number1 * number2;
+    case '+': return number1 + number2;
+    case '-': return number1 - number2;
     default: return "It's not correct operation. Please use '*' or '+' or '-'";
   }
 };
 
-const gameCalc = () => {
+const calculator = () => {
   const operation = operations[randomNumber(0, operations.length - 1)];
   const num1 = randomNumber(1, 100);
   const num2 = randomNumber(1, 100);
   const question = `${num1} ${operation} ${num2}`;
-  const correctAnswer = String(calculator(num1, num2, operation));
+  const correctAnswer = String(calculation(num1, num2, operation));
   return [correctAnswer, question, gameMessage];
 };
 
-const calc = () => generateGame(gameCalc);
+const calc = () => generateGame(calculator);
 
 export default calc;
